308076011511593f87aa89aad44709d0501708a3
Colin Swenson-Healey <chozandrias76@gmail.com>
2016-11-23 14:09:13 -0800

    Update

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
  * Created by colin on 11/19/16.
  */
 module.exports = "";
-//require("./node_modules/shopify-buy/lib/shopify.js");
+require("./node_modules/shopify-buy/lib/shopify.js");
 var jQuery = require("./node_modules/jquery/dist/jquery.js");
 
 require('./node_modules/jquery/dist/jquery.js');
@@ -11,14 +11,15 @@ const ShopifyBuy = require('./node_modules/shopify-buy/lib/shopify.js');
 const client = ShopifyBuy.buildClient({
     apiKey: 'f433b939b151eb13939281a30b9db245',
     appId: '6',
-    domain: '3d-printers-portal.myshopify.'
+    domain: '3d-printers-portal.myshopify.com'
 });
 
 $(document).ready(function () {
-   $.ajax({
+   $("#carousel").css('min-height', $(window).height());
+    $.ajax({
         type: "GET",
         dataType: "json",
-        url: "https://www.thingiverse.com/users/chozandrias/",
+        url: "https://api.thingiverse.com/users/chozandrias/",
         success: function(data){
             console.log(data);
         }

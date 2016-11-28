42ced56647c1dff73257d93b2823d63c19af5f3d
Colin Swenson-Healey <chozandrias76@gmail.com>
2016-11-28 14:59:59 -0800

    Got the thingiverse API to work. Updated front page content.

--- content.js
+++ content.js
@@ -19,13 +19,41 @@ $(document).ready(function () {
     $.ajax({
         type: "GET",
         dataType: "json",
-        url: "https://api.thingiverse.com/users/chozandrias/",
+        url: "https://api.thingiverse.com/popular/",
+        headers: {
+            "Authorization": "Bearer 33c4a53f6f6244c03008cf5f40e6e109"
+       },
         success: function(data){
-            console.log(data);
+            var divs = $("#splashProducts div img");
+
+            for(var i = 0; i < divs.length; i++){
+                drawPicture(i, data);
+            }
         }
     });
+    //console.log($("#splashProducts").toArray().each);
 });
 
+var drawPicture = function(divIndex, data){
+    var divs = $("#splashProducts div img");
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: data[divIndex].url + "images",
+        headers: {
+            "Authorization": "Bearer 33c4a53f6f6244c03008cf5f40e6e109"
+        },
+        success: function(imageData){
+
+            //console.log(imageData.default_image.url);
+            $("#splashProducts div img")[divIndex].src=imageData.default_image.url;
+            //$("#splashProducts div img")[divIndex].width=180;
+            $("#splashProducts div img")[divIndex].height=250;
+            //console.log(imageData);
+        }
+    });
+};
 
 client.fetchProduct('7785801291').then(function(product) {
     //console.log(product);
